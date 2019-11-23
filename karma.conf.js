d6bb603cd542dac24961f410c271400201ef39fc
Girma Nigusse <xgirma@gmail.com>
2019-11-22 21:25:12 -0500

    add karma-spec-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,17 +10,18 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-spec-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/Testing-Angular-Tour-Of-Heroes'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['spec', 'html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
