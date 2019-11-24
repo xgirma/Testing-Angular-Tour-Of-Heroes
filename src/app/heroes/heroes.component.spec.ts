5e652c03e4daa36f6817535c7d121fa2b70abbd7
Girma Nigusse <xgirma@gmail.com>
2019-11-24 18:40:58 -0500

    unit test

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -36,9 +36,44 @@ describe('HeroesComponent', () => {
     expect(compiled.querySelector('h2').textContent).toContain('WINDSTORM Details');
   });
 
-  it('should have id and name', () => {
+  // it('should have id and name', () => {
+  it('should have id', () => {
     const myHero = fixture.debugElement.queryAll(By.css('div'));
     expect(myHero[0].nativeElement.textContent).toEqual('id: 1');
-    expect(myHero[1].nativeElement.textContent).toEqual('name: Windstorm');
+    // expect(myHero[1].nativeElement.textContent).toEqual('name: Windstorm');
+  });
+});
+
+describe('HeroesComponent: input', () => {
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
+  let compiled: any;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule ],
+      declarations: [ HeroesComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
+  });
+
+  it(`should have editable hero name`, () => {
+    const inputBox = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputBox.value = 'Dr. Nice';
+    inputBox.dispatchEvent(new Event('input'));
+
+    expect(inputBox.value).toBe('Dr. Nice');
+    fixture.detectChanges();
+
+    // two way binding
+    expect(compiled.querySelector('h2').textContent)
+      .toEqual((component.hero.name).toUpperCase() + ' Details');
   });
 });
