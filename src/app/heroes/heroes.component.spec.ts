8481e1819b014393a18b2e06422df92d12974bea
Girma Nigusse <xgirma@gmail.com>
2019-11-24 22:13:22 -0500

    heroes

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -30,22 +30,7 @@ describe('HeroesComponent', () => {
   });
 
   it(`should have a hero`, () => {
-    expect(component.hero).toBeDefined();
-  });
-
-  it('should have title', () => {
-    const myHero = fixture.debugElement.queryAll(By.css('h2'));
-    expect(myHero[1].nativeElement.textContent).toContain('WINDSTORM Details');
-  });
-
-  it('should have id', () => {
-    const myHero = fixture.debugElement.queryAll(By.css('div'));
-    expect(myHero[0].nativeElement.textContent).toEqual('id: 1');
-  });
-
-  it('should have title', () => {
-    const heroes = fixture.debugElement.queryAll(By.css('h2'));
-    expect(heroes[0].nativeElement.textContent).toEqual('My Heroes');
+    expect(component.heroes).toBeDefined();
   });
 
   it('should have a list of heroes', () => {
@@ -54,6 +39,66 @@ describe('HeroesComponent', () => {
       expect(heroes[index].nativeElement.textContent).toContain(hero.id);
       expect(heroes[index].nativeElement.textContent).toContain(hero.name);
     });
+  });
+});
+
+describe('HeroesComponent: select', () => {
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
+  let compiled: any;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule ],
+      declarations: [ HeroesComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    component.heroes = HEROES;
+    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have title ${HEROES[0].name} Details`, async () => {
+    const myHero = fixture.debugElement.queryAll(By.css('.badge'));
+    myHero[0].nativeElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.selectedHero).toBeDefined();
+    const title = fixture.debugElement.queryAll(By.css('h2'));
+    expect(title[1].nativeElement.textContent)
+      .toEqual(`${(HEROES[0].name).toUpperCase()} Details`);
+  });
+
+  it(`should have id ${HEROES[0].id}`, async () => {
+    const myHero = fixture.debugElement.queryAll(By.css('.badge'));
+    myHero[0].nativeElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.selectedHero).toBeDefined();
+    expect(compiled.querySelector('div > div:nth-child(2)').textContent)
+      .toEqual(`id: ${HEROES[0].id}`);
+  });
+
+  it(`should have text '${HEROES[0].name}' in the input`, async () => {
+    const myHero = fixture.debugElement.queryAll(By.css('.badge'));
+    myHero[0].nativeElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.selectedHero).toBeDefined();
+    const inputBox = fixture.debugElement.query(By.css('input')).nativeElement;
+    expect(inputBox.value).toEqual(HEROES[0].name);
   });
 });
 
@@ -73,21 +118,28 @@ describe('HeroesComponent: input', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
+    component.heroes = HEROES;
     fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
   });
 
-  it(`should have editable hero name`, () => {
-    const inputBox = fixture.debugElement.query(By.css('input')).nativeElement;
-    inputBox.value = 'Dr. Nice';
-    inputBox.dispatchEvent(new Event('input'));
+  it('input should accept new value', async () => {
+    const myHero = fixture.debugElement.queryAll(By.css('.badge'));
+    myHero[0].nativeElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
 
-    expect(inputBox.value).toBe('Dr. Nice');
+    const inputBox = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputBox.value = 'Foo';
+    inputBox.dispatchEvent(new Event('input'));
+    expect(inputBox.value).toBe('Foo');
     fixture.detectChanges();
 
-    // two way binding
-    const myHero = fixture.debugElement.queryAll(By.css('h2'));
-    expect(myHero[1].nativeElement.textContent)
-      .toEqual((component.hero.name).toUpperCase() + ' Details');
+    expect(component.selectedHero).toBeDefined();
+    const title = fixture.debugElement.queryAll(By.css('h2'));
+    expect(title[1].nativeElement.textContent)
+      .toEqual(`${(HEROES[0].name).toUpperCase()} Details`);
+
+    expect(compiled.querySelector('li.selected').textContent).toContain('Foo');
   });
 });
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
