930af609d5397cfa6592c1197a542aea35f86285
Girma Nigusse <xgirma@gmail.com>
2019-11-24 19:53:42 -0500

    e2e

--- e2e/src/heroes.po.ts
+++ e2e/src/heroes.po.ts
@@ -2,8 +2,7 @@ import { browser, by, element, ExpectedConditions as EC } from 'protractor';
 
 export class AppHeroes {
   body = element(by.css('body'));
-  title = element(by.css('app-heroes > h2'));
-  // name = element.all(by.css('app-heroes > div'));
+  title = element.all(by.css('app-heroes > h2'));
   id = element.all(by.css('app-heroes > div'));
   name = element(by.css('input'));
 
@@ -13,17 +12,12 @@ export class AppHeroes {
   }
 
   getTitle() {
-    return this.title.getText() as Promise<any>;
+    return this.title.get(1).getText() as Promise<any>;
   }
 
   getId() {
-    // return this.name.get(0).getText() as Promise<any>;
     return this.id.get(0).getText() as Promise<any>;
   }
-
-  // getName() {
-  //   return this.name.get(1).getText() as Promise<any>;
-  // }
 
   setName(name) {
     this.name.clear();
