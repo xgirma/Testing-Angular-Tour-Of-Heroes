7ae935875aad447f9702872252f3ec369aa3ec44
Girma Nigusse <xgirma@gmail.com>
2019-11-26 07:22:47 -0500

    hero service

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,12 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
+// import { HEROES } from './mock-heroes';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let heroService: HeroService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    heroService = TestBed.get(HeroService);
+  });
 
   it('should be created', () => {
-    const service: HeroService = TestBed.get(HeroService);
-    expect(service).toBeTruthy();
+    expect(heroService).toBeTruthy();
   });
+
+  // it('should have heroes', () => {
+  //   expect(heroService.getHeroes()).toEqual(HEROES);
+  // });
+
+  // tslint:disable-next-line:no-shadowed-variable
+  it(`get all heroes`, inject([HeroService], (heroService) => {
+    heroService.getHeroes().subscribe(heroes => {
+      expect(heroes.length).toEqual(10);
+    });
+  }));
 });
