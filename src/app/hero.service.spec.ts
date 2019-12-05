9a63e3a6ce672f6a1694521bc378990c154640d1
Girma Nigusse <xgirma@gmail.com>
2019-12-05 05:21:47 -0500

    unit test

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,24 +1,43 @@
 import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
-import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 
 describe('HeroService', () => {
   let heroService: HeroService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HeroService]
+    });
     heroService = TestBed.get(HeroService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(heroService).toBeTruthy();
   });
 
-  // tslint:disable-next-line:no-shadowed-variable
-  it(`get all heroes`, inject([HeroService], (heroService) => {
+  it(`should get heroes data`, () => {
+    const mockHeroes: Hero[] = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' }
+    ];
     heroService.getHeroes().subscribe(heroes => {
-      expect(heroes).toEqual(HEROES);
+      expect(heroes.length).toEqual(3);
+      expect(heroes[0].id).toEqual(11);
+      expect(heroes[0].name).toEqual(`Dr Nice`);
     });
-  }));
+    const request = httpMock.expectOne( `api/heroes`, 'call to api');
+    expect(request.request.method).toBe('GET');
+    request.flush(mockHeroes);
+  });
 });
