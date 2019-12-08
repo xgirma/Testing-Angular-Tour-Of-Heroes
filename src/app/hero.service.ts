9bb3392941534f2901131854b36a3a39f4362177
Girma Nigusse <xgirma@gmail.com>
2019-12-07 21:34:38 -0500

    add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -37,11 +37,17 @@ export class HeroService {
       );
   }
 
-  /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
@@ -65,7 +71,6 @@ export class HeroService {
     };
   }
 
-  /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
